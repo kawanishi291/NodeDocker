9e4a318939fa1378129384935aac8fb93a8fc814
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-03-27 02:00:41 +0900

    MySQL接続情報分割

---
+++ src/connectdb/connectdb.js
@@ -0,0 +1,11 @@
+// MySQL設定
+module.exports = function() {
+    var mysql_setting = {
+        host    : 'a2abe6019571',
+        user    : 'admin',
+        password: 'adminpass',
+        database: 'sample_db'
+    };
+    
+    return mysql_setting;
+}
