871c78f599e2b688c13f0e6dcbaabb8cbe4bc263
kawanishi291 <71476154+kawanishi291@users.noreply.github.com>
2021-03-29 01:20:57 +0900

    会員登録 vue.js

---
+++ src/public/javascripts/signup_vue.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    new Vue({
+        el:'#app',
+        data: {
+            pass: '',
+            pass2: '',
+            passMsg: "",
+            email: '',
+            email2: '',
+            emailMsg: "",
+            submit: false
+        },
+        methods: {
+            checkpass: function(){
+                if (this.pass == this.pass2){
+                    this.passMsg = "";
+                    if(this.email == this.email2 && this.email != '' && this.email2 != ''){
+                        this.submit = true;
+                    }
+                }else{
+                    this.passMsg = "同じパスワードを入力してください。";
+                    this.submit = false;
+                }
+            },
+            checkeemail: function(){
+                if (this.email == this.email2){
+                    this.emailMsg = "";
+                    if(this.pass == this.pass2 && this.pass != '' && this.pass2 != ''){
+                        this.submit = true;
+                    }
+                }else{
+                    this.emailMsg = "同じメールアドレスを入力してください。";
+                    this.submit = false;
+                }
+            }
+        }
+    })
+});
